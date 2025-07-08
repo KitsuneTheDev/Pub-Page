7a6fb26f41f8ce1a81665f508666056fe0971978
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-07-08 07:32:43 +0300

    update: hero section
    - Hero section layout done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,8 +4,14 @@ export default function Hero() {
 
     return(
         <div 
-        className="hero-container h-ht-sm">
-            <h1 className="text-4xl text-white">HERO</h1>
+        className="hero-container h-ht-sm relative">
+            <p className='text-white/70 text-sm font-c-source font-c-light w-[80%] absolute left-[11%] bottom-[20%]
+            md:w-[70%] md:bottom-[20%]
+            lg:w-[40%] lg:bottom-[14%]
+            xl:w-[30%] xl:bottom-[5%]'>
+                Nestled in Tepebaşı, our pub is the perfect place to unwind, whether you're catching up with friends or enjoying a drink solo.
+                Great vibes, solid drink prices, and real Eskişehir spirit with good music and better company.
+            </p>
         </div>
     );
 }
